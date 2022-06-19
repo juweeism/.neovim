b3babffc381f6f55470903a33e457f2ad4087afa
jamibear <5701ja.mi@gmail.com>
2022-06-19 20:36:37 +0800

    install emmet lsp

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -20,7 +20,12 @@ lsp_installer.on_server_ready(function(server)
       local csharp_opts = require("user.lsp.settings.csharp_ls")
       opts = vim.tbl_deep_extend("force", csharp_opts, opts)
      end
-	-- This setup() function is exactly the same as lspconfig's setup function.
+
+  if server.name == "emmet_ls" then
+      local emmet_opts = require("user.lsp.settings.emmet_ls")
+      opts = vim.tbl_deep_extend("force", emmet_opts, opts)
+     end
+-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 	server:setup(opts)
 end)
