e314e10d85c268410957e8db33f9ed925c5befa4
jamibear <5701ja.mi@gmail.com>
2022-07-01 23:36:43 +0800

    fix tabs and indenting

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -21,9 +21,7 @@ local options = {
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = true,                  -- set relative numbered lines
@@ -33,6 +31,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  softtabstop = 4                          -- combine tabs and spaces
 }
 
 vim.opt.shortmess:append "c"
